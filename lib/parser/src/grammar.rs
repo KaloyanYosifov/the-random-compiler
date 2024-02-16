fe2d459b68e5276c61feb87d8517f5a1f948051b
KaloyanYosifov <kaloqn665@gmail.com>
2024-02-16 06:54:54 +0100

    Support additional production rules

--- lib/parser/src/grammar.rs
+++ lib/parser/src/grammar.rs
@@ -12,14 +12,23 @@ pub enum NonTerminal {
     StatementPrime,
     #[strum(serialize = "A")]
     AssignmentStatement,
+    #[strum(serialize = "E")]
+    Expression,
     #[strum(serialize = "K")]
     Keyword,
+    #[strum(serialize = "V")]
+    Variable,
+    #[strum(serialize = "Q")]
+    Conditional,
+    #[strum(serialize = "F")]
+    ForLoop,
 }
 
 pub enum ProductionRuleSymbol {
     Token(Token),
     NonTerminal(NonTerminal),
     TokenClass(TokenClass),
+    Empty,
 }
 
 pub type ProductionRule = Vec<ProductionRuleSymbol>;
@@ -35,6 +44,13 @@ impl Grammar {
         let mut grammar = HashMap::new();
 
         Self::init_program_production_rules(&mut grammar);
+        Self::init_statement_production_rules(&mut grammar);
+        Self::init_assignment_statement_production_rules(&mut grammar);
+        Self::init_expression_production_rules(&mut grammar);
+        Self::init_conditional_production_rules(&mut grammar);
+        Self::init_for_loop_production_rules(&mut grammar);
+        Self::init_keyword_production_rules(&mut grammar);
+        Self::init_variable_production_rules(&mut grammar);
 
         Self { grammar }
     }
@@ -42,22 +58,79 @@ impl Grammar {
     pub fn init_program_production_rules(table: &mut GrammarTable) {
         table.insert(
             NonTerminal::Program,
-            vec![vec![ProductionRuleSymbol::NonTerminal(NonTerminal::Statement)]],
+            vec![vec![ProductionRuleSymbol::NonTerminal(
+                NonTerminal::Statement,
+            )]],
         );
     }
 
     pub fn init_statement_production_rules(table: &mut GrammarTable) {
         table.insert(
             NonTerminal::Statement,
-            vec![vec![
-                ProductionRuleSymbol::NonTerminal(NonTerminal::AssignmentStatement),
-                ProductionRuleSymbol::NonTerminal(NonTerminal::StatementPrime),
-            ]],
+            vec![
+                vec![
+                    ProductionRuleSymbol::NonTerminal(NonTerminal::AssignmentStatement),
+                    ProductionRuleSymbol::NonTerminal(NonTerminal::StatementPrime),
+                ],
+                vec![
+                    ProductionRuleSymbol::NonTerminal(NonTerminal::Variable),
+                    ProductionRuleSymbol::TokenClass(TokenClass::Lparen),
+                    ProductionRuleSymbol::NonTerminal(NonTerminal::Expression),
+                    ProductionRuleSymbol::TokenClass(TokenClass::Rparen),
+                    ProductionRuleSymbol::NonTerminal(NonTerminal::StatementPrime),
+                ],
+            ],
         );
     }
 
     pub fn init_assignment_statement_production_rules(table: &mut GrammarTable) {
-        table.insert(NonTerminal::AssignmentStatement, vec![vec![]]);
+        table.insert(
+            NonTerminal::AssignmentStatement,
+            vec![vec![
+                ProductionRuleSymbol::NonTerminal(NonTerminal::Keyword),
+                ProductionRuleSymbol::NonTerminal(NonTerminal::Variable),
+                ProductionRuleSymbol::TokenClass(TokenClass::Assignment),
+                ProductionRuleSymbol::NonTerminal(NonTerminal::Expression),
+                ProductionRuleSymbol::TokenClass(TokenClass::Semi),
+            ]],
+        );
+    }
+
+    pub fn init_conditional_production_rules(table: &mut GrammarTable) {
+        table.insert(
+            NonTerminal::Conditional,
+            vec![vec![
+                ProductionRuleSymbol::NonTerminal(NonTerminal::Keyword),
+                ProductionRuleSymbol::TokenClass(TokenClass::Lparen),
+                ProductionRuleSymbol::NonTerminal(NonTerminal::Expression),
+                ProductionRuleSymbol::TokenClass(TokenClass::Rparen),
+                ProductionRuleSymbol::TokenClass(TokenClass::LCurly),
+                ProductionRuleSymbol::NonTerminal(NonTerminal::Statement),
+                ProductionRuleSymbol::TokenClass(TokenClass::RCurly),
+            ]],
+        );
+    }
+
+    pub fn init_for_loop_production_rules(table: &mut GrammarTable) {
+        table.insert(
+            NonTerminal::Conditional,
+            vec![vec![
+                ProductionRuleSymbol::Token(Token::Keyword("for".to_owned())),
+                ProductionRuleSymbol::TokenClass(TokenClass::Lparen),
+                ProductionRuleSymbol::NonTerminal(NonTerminal::AssignmentStatement),
+                ProductionRuleSymbol::NonTerminal(NonTerminal::Expression),
+                ProductionRuleSymbol::TokenClass(TokenClass::Semi),
+                ProductionRuleSymbol::NonTerminal(NonTerminal::Expression),
+                ProductionRuleSymbol::TokenClass(TokenClass::Rparen),
+                ProductionRuleSymbol::TokenClass(TokenClass::LCurly),
+                ProductionRuleSymbol::NonTerminal(NonTerminal::Statement),
+                ProductionRuleSymbol::TokenClass(TokenClass::RCurly),
+            ]],
+        );
+    }
+
+    pub fn init_expression_production_rules(_table: &mut GrammarTable) {
+        todo!("Implement expression production rules!");
     }
 
     pub fn init_keyword_production_rules(table: &mut GrammarTable) {
@@ -71,5 +144,14 @@ impl Grammar {
             .collect();
 
         table.insert(NonTerminal::Keyword, production_rules);
+    }
+
+    pub fn init_variable_production_rules(table: &mut GrammarTable) {
+        table.insert(
+            NonTerminal::Variable,
+            vec![vec![ProductionRuleSymbol::TokenClass(
+                TokenClass::Identifier,
+            )]],
+        );
     }
 }
